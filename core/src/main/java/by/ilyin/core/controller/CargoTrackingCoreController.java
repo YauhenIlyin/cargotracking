dd3aee3ee427d8b427a6cf177d56bccb0b02152d
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-09-27 19:09:40 +0300

    CTB-3/cors config created

--- core/src/main/java/by/ilyin/core/controller/CargoTrackingCoreController.java
+++ core/src/main/java/by/ilyin/core/controller/CargoTrackingCoreController.java
@@ -3,9 +3,7 @@ package by.ilyin.core.controller;
 import by.ilyin.core.entity.dto.AppInfoDTO;
 import by.ilyin.core.service.AppInfoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,7 +14,6 @@ public class CargoTrackingCoreController {
 
     @GetMapping("/about")
     public AppInfoDTO getAppInfo() {
-        System.out.println(appInfoService);
         AppInfoDTO appInfoDTO;
         appInfoDTO = appInfoService.getAppInfo();
         return appInfoDTO;
