6e7719c3f03a5b0d784f13c31fab36b4659211b3
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-19 19:56:11 +0300

    CTB-7_Error handling is created

--- core/src/main/java/by/ilyin/core/repository/CustomUserRepository.java
+++ core/src/main/java/by/ilyin/core/repository/CustomUserRepository.java
@@ -15,6 +15,4 @@ public interface CustomUserRepository extends JpaRepository<CustomUser, Long>, J
 
     Optional<CustomUser> findByEmail(String email);
 
-    Boolean existsByEmail(String email);
-
 }
