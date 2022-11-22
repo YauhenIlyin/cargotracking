4231522aa020d7f11d6c02f289c7fa63c2a060fa
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-22 20:25:13 +0300

    CTB-6_JWT_authentication is created

--- web/src/main/java/by/ilyin/web/controller/AuthController.java
+++ web/src/main/java/by/ilyin/web/controller/AuthController.java
@@ -11,6 +11,8 @@ import javax.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.constraints.NotNull;
 
 @RequiredArgsConstructor
--- web/src/main/java/by/ilyin/web/util/error/CustomControllerAdvice.java
+++ web/src/main/java/by/ilyin/web/util/error/CustomControllerAdvice.java
@@ -444,5 +444,4 @@ public class CustomControllerAdvice {
                 httpStatus
         );
     }
-
 }
