25e34ab1741011c98408cae07a9f19fc0adf5866
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-22 20:25:13 +0300

    CTB-6_JWT_authentication is created

--- web/src/main/java/by/ilyin/web/config/SecurityConfig.java
+++ web/src/main/java/by/ilyin/web/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+
     }
 
 }
