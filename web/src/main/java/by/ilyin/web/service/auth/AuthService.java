8bf56a1750c02e9e44ebc833c09ba9431a01f950
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-22 20:25:13 +0300

    CTB-6_JWT_authentication is created

--- web/src/main/java/by/ilyin/web/service/auth/AuthService.java
+++ web/src/main/java/by/ilyin/web/service/auth/AuthService.java
@@ -26,6 +26,7 @@ import by.ilyin.web.security.JwtBlackListManager;
 import by.ilyin.web.util.validator.CustomBindingResultValidator;
 import by.ilyin.web.security.JwtBlackListManager;
 import by.ilyin.web.util.validator.CustomBindingResultValidator;
+import by.ilyin.web.security.JwtBlackListManager;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
