b6027f06357cd8b3f128e16c4fa948a157b10028
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-22 20:25:13 +0300

    CTB-6_JWT_authentication is created

---
+++ core/src/main/java/by/ilyin/core/controller/AuthController.java
@@ -0,0 +1,48 @@
+package by.ilyin.core.controller;
+
+import by.ilyin.core.dto.SignInDTO;
+import by.ilyin.core.entity.CustomJWT;
+import by.ilyin.core.entity.CustomUser;
+import by.ilyin.core.service.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-in")
+    public CustomUser signIn(@RequestBody SignInDTO signInDTO) {
+        return authService.getUserByCredentials(signInDTO);
+    }
+
+    @PostMapping("/logout")
+    public Set<CustomJWT> logout(@RequestParam(value = "userId") Long userId) {
+        return authService.logoutProcess(userId);
+    }
+
+    @GetMapping("/jwt")
+    public Set<CustomJWT> getUpToDateBlackList() {
+        return authService.getJwtBlackList();
+    }
+
+    @PostMapping("/jwt")
+    public ResponseEntity<Void> saveJWT(@RequestBody CustomJWT customJWT) {
+        authService.saveJWT(customJWT);
+        return ResponseEntity
+                .ok()
+                .build();
+    }
+
+    @PutMapping("/jwt")
+    public Set<CustomJWT> blockAccessJwt(@RequestBody CustomJWT refreshJWT) {
+        return authService.blockAccessJWT(refreshJWT);
+    }
+
+}
