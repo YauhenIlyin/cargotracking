cc50ce7c84d8e11e2a1b6604c508a5881bd8cb99
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-22 20:25:13 +0300

    CTB-6_JWT_authentication is created

---
+++ core/src/main/resources/db/changelog/004_crud_clients.postgresql.sql
@@ -0,0 +1,20 @@
+--liquibase formatted sql
+
+--changeset part1:create-clients
+--preconditions onFail:MARK_RAN onError:MARK_RAN
+--precondition tableExists public.clients
+CREATE TABLE public.clients
+(
+    id          BIGINT PRIMARY KEY NOT NULL,
+    client_name VARCHAR(20)        NOT NULL,
+    status      VARCHAR(20)        NOT NULL,
+    admin_id    BIGINT,
+    delete_date  TIMESTAMP DEFAULT NULL,
+    FOREIGN KEY (admin_id) REFERENCES public.users (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+--changeset part2:alter-table-users-add_fk_clientId
+--preconditions onFail:MARK_RAN onError:MARK_RAN
+--precondition tableExists public.users
+ALTER TABLE users
+    ADD CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES clients (id);
