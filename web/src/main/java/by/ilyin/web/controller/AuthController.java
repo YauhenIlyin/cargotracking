82d6ac495fbb97f4fc26128b8cf00c353956e5c3
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-22 20:25:13 +0300

    CTB-6_JWT_authentication is created

--- web/src/main/java/by/ilyin/web/controller/AuthController.java
+++ web/src/main/java/by/ilyin/web/controller/AuthController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.constraints.NotNull;
 
 @RequiredArgsConstructor
