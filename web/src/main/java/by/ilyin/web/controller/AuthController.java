89947547be686a9caa7128d795c7f750e03d160c
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-23 10:59:36 +0300

    CTB-6 added login password check during sign in process

--- web/src/main/java/by/ilyin/web/controller/AuthController.java
+++ web/src/main/java/by/ilyin/web/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @RequiredArgsConstructor
