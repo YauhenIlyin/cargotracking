979f7978c0a70fd9d502e04c97242338cca26cf1
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-23 10:59:36 +0300

    CTB-6 added login password check during sign in process

--- web/src/main/java/by/ilyin/web/controller/AuthController.java
+++ web/src/main/java/by/ilyin/web/controller/AuthController.java
@@ -1,11 +1,14 @@
 package by.ilyin.web.controller;
 
+import by.ilyin.web.dto.auth.LogoutDTO;
+import by.ilyin.web.dto.auth.RefreshJwtDTO;
 import by.ilyin.web.dto.auth.SignInDTO;
 import by.ilyin.web.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @RequiredArgsConstructor
@@ -18,19 +21,17 @@ public class AuthController {
     //todo add @Valid
     //todo string to byte[];
     @PostMapping("/sign-in")
-    public String signIn(SignInDTO signInDTO) {
+    public String signIn(@RequestBody @Valid SignInDTO signInDTO) {
         return authService.signInProcess(signInDTO);
     }
 
     @PostMapping("/refresh")
-    public String refresh(@RequestParam(value = "userId") @NotNull Long userId,
-                          @RequestParam(value = "token") String token) {
-        return authService.refreshProcess(userId, token);
+    public String refresh(@RequestBody @Valid RefreshJwtDTO refreshJwtDTO) {
+        return authService.refreshProcess(refreshJwtDTO);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestParam(value = "userId") Long userId) {
-        authService.logoutProcess(userId);
+    public ResponseEntity<Void> logout(@RequestBody @Valid LogoutDTO logoutDTO) {
         return ResponseEntity
                 .ok()
                 .build();
