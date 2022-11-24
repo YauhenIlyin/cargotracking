5f8bdf7912664918420f1239fe7939d3ccf10e6d
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-24 10:16:22 +0300

    CTB-8_work with profiles added

--- web/src/main/java/by/ilyin/web/config/SecurityConfig.java
+++ web/src/main/java/by/ilyin/web/config/SecurityConfig.java
@@ -90,7 +90,6 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-
     }
 
 }
