d2c666823c7e605902a85c899877639a37273e1e
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-24 10:16:22 +0300

    CTB-8_work with profiles added

--- web/src/main/java/by/ilyin/web/config/SecurityConfig.java
+++ web/src/main/java/by/ilyin/web/config/SecurityConfig.java
@@ -26,28 +26,21 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.GET, "/api/users").hasAnyRole("ADMIN", "DISPATCHER")
                 .antMatchers(HttpMethod.POST, "/api/users/{id}").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/users/{id}").hasRole("ADMIN")
-<<<<<<< HEAD
 
                 .antMatchers(HttpMethod.GET, "/api/profile").hasAnyRole("COMPANY_OWNER", "DISPATCHER", "MANAGER", "DRIVER")
                 .antMatchers(HttpMethod.PUT, "/api/profile").hasAnyRole("COMPANY_OWNER", "DISPATCHER", "MANAGER", "DRIVER")
                 .antMatchers(HttpMethod.PUT, "/api/profile/change-password").hasAnyRole("COMPANY_OWNER", "DISPATCHER", "MANAGER", "DRIVER")
-                
+
                 .antMatchers("/api/sign-in", "api/logout", "/api/refresh").permitAll()
 
                 .antMatchers(HttpMethod.POST, "/api/email").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/email/repairing").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/restore").permitAll()
 
-
-=======
-                .antMatchers("api/logout").authenticated()
-                .antMatchers("/api/sign-in", "/api/refresh").permitAll()
->>>>>>> b5f572a (CTB-6_JWT_authentication is created)
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-
     }
 
 }
