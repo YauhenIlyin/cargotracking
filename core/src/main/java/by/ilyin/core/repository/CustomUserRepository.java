cc685d426c77f870c660dbf4ff2f5392a6a36706
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-25 20:45:52 +0300

    CTB-16 Restore account mechanism added

--- core/src/main/java/by/ilyin/core/repository/CustomUserRepository.java
+++ core/src/main/java/by/ilyin/core/repository/CustomUserRepository.java
@@ -17,6 +17,6 @@ public interface CustomUserRepository extends JpaRepository<CustomUser, Long>, J
 
     Optional<CustomUser> findByEmail(String email);
 
-    Boolean existsByEmail(String recipient);
+    Boolean existsByEmail(String email);
 
 }
--- web/src/main/java/by/ilyin/web/controller/EmailWebController.java
+++ web/src/main/java/by/ilyin/web/controller/EmailWebController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-
 @RequiredArgsConstructor
 @Validated
 @RestController
