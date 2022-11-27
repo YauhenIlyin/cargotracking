79f77a6cde3fffab907c26355b1a84f8e4935063
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-27 21:47:34 +0300

    CTB-9 CRUD Clients added

--- web/src/main/java/by/ilyin/web/controller/ClientWebController.java
+++ web/src/main/java/by/ilyin/web/controller/ClientWebController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
