c912eaa95ad38079c05eb80abefade396e8e3af5
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-11-30 13:26:36 +0300

    CTB-10 CRUD Storage created

--- core/src/main/java/by/ilyin/core/entity/Storage.java
+++ core/src/main/java/by/ilyin/core/entity/Storage.java
@@ -1,6 +1,5 @@
 package by.ilyin.core.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -23,8 +22,6 @@ public class Storage extends BaseEntity {
     private String name;
     @Column(name = "address")
     private String address;
-    @JsonIgnore
-    @ToString.Exclude
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "client_id", referencedColumnName = "id")
     private Client client;
