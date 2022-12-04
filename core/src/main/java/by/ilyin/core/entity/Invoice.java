cf02d867bd9a0152b9e0607a4951c87b305573d0
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-12-04 21:03:13 +0300

    CTB-13 small fix for hibernate entities

--- core/src/main/java/by/ilyin/core/entity/Invoice.java
+++ core/src/main/java/by/ilyin/core/entity/Invoice.java
@@ -1,6 +1,5 @@
 package by.ilyin.core.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -56,6 +55,58 @@ public class Invoice extends BaseEntity {
         VERIFICATION_COMPLETE
     }
     
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Invoice invoice = (Invoice) o;
+        if (!Objects.equals(id, invoice.id)) return false;
+        if (!Objects.equals(number, invoice.number)) return false;
+        if (!Objects.equals(creationDate, invoice.creationDate))
+            return false;
+        if (!Objects.equals(verificationDate, invoice.verificationDate))
+            return false;
+        if (status != invoice.status) return false;
+        if (!Objects.equals(products, invoice.products)) return false;
+        if (!Objects.equals(storage, invoice.storage)) return false;
+        if (!Objects.equals(productOwner, invoice.productOwner))
+            return false;
+        if (!Objects.equals(creator, invoice.creator)) return false;
+        return Objects.equals(driver, invoice.driver);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        result = 31 * result + (creationDate != null ? creationDate.hashCode() : 0);
+        result = 31 * result + (verificationDate != null ? verificationDate.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (products != null ? products.hashCode() : 0);
+        result = 31 * result + (storage != null ? storage.hashCode() : 0);
+        result = 31 * result + (productOwner != null ? productOwner.hashCode() : 0);
+        result = 31 * result + (creator != null ? creator.hashCode() : 0);
+        result = 31 * result + (driver != null ? driver.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Invoice{");
+        sb.append("id=").append(id);
+        sb.append(", number='").append(number).append('\'');
+        sb.append(", creationDate=").append(creationDate);
+        sb.append(", verificationDate=").append(verificationDate);
+        sb.append(", status=").append(status);
+        sb.append(", products=").append(products);
+        sb.append(", storage=").append(storage);
+        sb.append(", productOwner=").append(productOwner);
+        sb.append(", creator=").append(creator);
+        sb.append(", driver=").append(driver);
+        sb.append('}');
+        return sb.toString();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
