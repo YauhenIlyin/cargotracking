050c135085dd7d4982189e762c467bc500ede909
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-12-19 02:00:10 +0300

    CTB-15 small bugs fixed

--- core/src/main/java/by/ilyin/core/service/EmailService.java
+++ core/src/main/java/by/ilyin/core/service/EmailService.java
@@ -2,7 +2,7 @@ package by.ilyin.core.service;
 
 import by.ilyin.core.dto.SendEmailDTO;
 import by.ilyin.core.dto.mapper.EmailDTOMapper;
-import by.ilyin.core.util.email.EmailProcessService;
+import by.ilyin.core.util.email.EmailProcessManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final EmailProcessService emailProcessService;
+    private final EmailProcessManager emailProcessManager;
     private final EmailDTOMapper emailDTOMapper;
 
     public void sendEmail(SendEmailDTO sendEmailDTO) {
-        emailProcessService.sendSimpleMail(emailDTOMapper.mapFromDto(sendEmailDTO));
+        emailProcessManager.sendSimpleMail(emailDTOMapper.mapFromDto(sendEmailDTO));
     }
 
 }
