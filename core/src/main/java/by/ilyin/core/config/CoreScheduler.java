f8d3b4ef24f20529d1931714516bab1da9e0264d
Yauhen Ilyin <yauhen.ilyin@gmail.com>
2022-12-19 01:43:18 +0300

    CTB-6 sheduler renamed

--- core/src/main/java/by/ilyin/core/config/CoreScheduler.java
+++ core/src/main/java/by/ilyin/core/config/CoreScheduler.java
@@ -1,23 +1,25 @@
-package by.ilyin.web.config;
+package by.ilyin.core.config;
 
-import by.ilyin.web.security.JwtBlackListManager;
+import by.ilyin.core.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 @RequiredArgsConstructor
-public class SchedulerConfig {
+public class CoreScheduler {
 
-    private final JwtBlackListManager jwtBlackListManager;
+    private final AuthService authService;
 
     @Async
     @Scheduled(fixedDelay = 1000 * 60 * 60 * 3)
-    public void getUpToDateBlackList() {
-        jwtBlackListManager.updateBlackList();
+    public void cleanExpiredTokens() {
+        authService.cleanUpExpiredJWT(LocalDateTime.now());
     }
 
 }
--- web/src/main/java/by/ilyin/web/config/WebScheduler.java
+++ web/src/main/java/by/ilyin/web/config/WebScheduler.java
@@ -1,25 +1,23 @@
-package by.ilyin.core.config;
+package by.ilyin.web.config;
 
-import by.ilyin.core.service.auth.AuthService;
+import by.ilyin.web.security.JwtBlackListManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.time.LocalDateTime;
-
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 @RequiredArgsConstructor
-public class SchedulerConfig {
+public class WebScheduler {
 
-    private final AuthService authService;
+    private final JwtBlackListManager jwtBlackListManager;
 
     @Async
     @Scheduled(fixedDelay = 1000 * 60 * 60 * 3)
-    public void cleanExpiredTokens() {
-        authService.cleanUpExpiredJWT(LocalDateTime.now());
+    public void getUpToDateBlackList() {
+        jwtBlackListManager.updateBlackList();
     }
 
 }
